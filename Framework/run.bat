0aac53da37c86d2b15ad8aeb8a4edd71db366300
Jamshid Mozafari <93037500+JamshidJDMY@users.noreply.github.com>
2024-02-06 19:33:23 +0100

    Delete Framework/run.bat

--- Framework/run.bat
+++
@@ -1,3 +0,0 @@
-python Pipeline.py --help 
-python Pipeline.py --generation_api_key esecret_rcubjjp5n715e4b4gkw791sjsk --evaluation_api_key esecret_rcubjjp5n715e4b4gkw791sjsk --input_file sample.json --output_file sample_output.json --model LLaMA_70b_Vanilla --num_of_hints 7
-pause
